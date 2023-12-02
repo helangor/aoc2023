5ece9c4aa4ffdec1891a11ffb909c12ea2c5a6f2
Henrikki Helander <henrikki_helander@hotmail.com>
2023-12-02 17:14:15 +0200

    solved task1 day2

---
+++ day2/day2.py
@@ -0,0 +1,44 @@
+def read_file(file_path):
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+    return lines
+
+"""Etsii pelin suurimman saman väristen määrän"""
+def parse_line(line):
+    parts = line.split(':')
+    game_id = parts[0].split()[1] 
+    gems = {}
+    gem_parts = ''.join(parts[1:]).split(';')
+    for gem_part in gem_parts:
+        gem_info = gem_part.split(',')
+        for info in gem_info:
+            count, color = info.split()
+            if color not in gems or int(count) > gems[color]:
+                gems[color] = int(count)
+    return game_id, gems
+
+"""Peli mahdollinen, jos pelin kuulia tietylle värille ei ole enempää kuin haluttu määrä"""
+def is_game_possible(wanted_cubes, line_cubes):
+    for color, count in wanted_cubes.items():
+        if line_cubes[color] > count:
+            return False
+    return True
+
+def task1(lines):
+    total_game_id_sum = 0
+    wanted_cubes = {"red": 12, "green": 13, "blue": 14}
+
+    for line in lines:
+        game_id, game_gems = parse_line(line.strip())
+        if is_game_possible(wanted_cubes, game_gems):
+            total_game_id_sum += int(game_id)
+
+    print(total_game_id_sum)
+
+def main():
+    file_path = "C:\\Users\\henri\\Desktop\\aoc\\day2\\day2.txt"
+    lines = read_file(file_path)
+    task1(lines)
+
+if __name__ == "__main__":
+    main()
